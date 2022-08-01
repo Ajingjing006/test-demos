23368c8d5fb2b0ddc88f81bf3e032872d132f38e
Ajingjing006 <1028126924@qq.com>
2022-08-01 13:51:30 +0800

    123123
    123

---
+++ require/require.config.js
@@ -0,0 +1,14 @@
+requirejs.config({
+	baseUrl: '.',
+	waitSeconds: 30,
+	// deps: ["migrate"],
+	paths: {
+		'jquery': 'jq/jquery-3.6.0.min',
+		'migrate': 'jq/jquery-migrate'
+	},
+	shim: {
+		// 'jquery': {
+		// 	deps: ['migrate']
+		// }
+	}
+});
